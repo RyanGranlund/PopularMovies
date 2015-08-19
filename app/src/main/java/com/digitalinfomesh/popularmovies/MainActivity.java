5a20244746e2256dbf238e4d3b192195eb0e143e
Ryan Granlund <ryangranlund@Ryans-MacBook-Pro.local>
2015-08-18 20:30:24 -0700

    Added Settings Activity

--- app/src/main/java/com/digitalinfomesh/popularmovies/MainActivity.java
+++ app/src/main/java/com/digitalinfomesh/popularmovies/MainActivity.java
@@ -1,5 +1,6 @@
 package com.digitalinfomesh.popularmovies;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -30,9 +31,9 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/digitalinfomesh/popularmovies/SettingsActivity.java
+++ app/src/main/java/com/digitalinfomesh/popularmovies/SettingsActivity.java
@@ -4,20 +4,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 
-public class DetailActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.activity_settings);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_detail, menu);
+        //getMenuInflater().inflate(R.menu.menu_settings, menu);
         return true;
     }
+
 
 }
